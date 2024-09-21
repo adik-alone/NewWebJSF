bc02fb43cb32672fd4412ef528d278ab9f137c7b
Adik-Alone <vasyatsalov@gmail.com>
2024-09-21 21:22:16 +0300

    fix junit tests: junit ver. 4

--- src/test/ShootBeanTest.java
+++ src/test/ShootBeanTest.java
@@ -1,33 +1,46 @@
 import com.example.newwebjsf.shootbean.ResultBean;
 import com.example.newwebjsf.shootbean.Shot;
 import jakarta.inject.Inject;
-import org.junit.jupiter.api.Assertions;
-//import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
 
-//@DisplayName("Shoot bean test for check area")
 public class ShootBeanTest {
-//    @Inject
-//    Shot bean;
-//
-//    @Inject
-//    ResultBean resultBean;
 
     @Test
-    public void zeroCheckAreaTest(){
-        CheckAreaInShotBeanTest("true", 0.0f, 0.0f, 0.0f);
-    }
-    private void CheckAreaInShotBeanTest(String expectedResult, Float x, Float y, Float r){
-        //setup
-        Shot bean = new Shot();
-        bean.setX(x);
-        bean.setY(y);
-        bean.setR(r);
+    public void testCheckAreaZero() {
         ResultBean resultBean = new ResultBean();
-        //execute
-        boolean result = resultBean.CheckArea(bean);
-        //Assertion
-        Assertions.assertEquals(expectedResult, Boolean.toString(result));
+        Shot bean = new Shot();
+        bean.setX(0);
+        bean.setY(0);
+        bean.setR(0);
+        assertTrue(resultBean.CheckArea(bean));
+    }
+    @Test
+    public void testCheckAreaTrue() {
+        ResultBean resultBean = new ResultBean();
+        Shot bean = new Shot();
+        bean.setX(0);
+        bean.setY(0);
+        bean.setR(1);
+        assertTrue(resultBean.CheckArea(bean));
+    }
+    @Test
+    public void testCheckAreaFalse() {
+        ResultBean resultBean = new ResultBean();
+        Shot bean = new Shot();
+        bean.setX(13);
+        bean.setY(15);
+        bean.setR(1);
+        assertFalse(resultBean.CheckArea(bean));
+    }
+    @Test
+    public void testCheckAreaTrue_2() {
+        ResultBean resultBean = new ResultBean();
+        Shot bean = new Shot();
+        bean.setX(3);
+        bean.setY(4);
+        bean.setR(5);
+        assertTrue(resultBean.CheckArea(bean));
     }
 }
